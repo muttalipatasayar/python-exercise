7a4ef7c3081abfee97633ae90f6f9a90a8873579
muttalip atasayar <muttalipatasayar@gmail.com>
2021-01-02 22:02:16 +0300

    Add files via upload

---
+++ oyuncu_kaydetme.py
@@ -0,0 +1,8 @@
+print("oyuncu kaydetme programı")
+ad= input("oyunucunun Adı : ")
+soyad= input("oyuncunun soyadı : ")
+takım =input("oyuncunun takımı : ")
+bilgiler=[ad,soyad,takım]
+print ("bilgiler kaydediliyor...")
+print("oyuncunun adı :{}\n oyuncunun soyadı : {}\n oyuncunun takımı : {}\n ".format(bilgiler[0],bilgiler[1],bilgiler[2]))
+print("bilgiler kaydedildi ......")
