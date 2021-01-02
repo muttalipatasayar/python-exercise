305b562d5ab35c22835d8fb92cc8d92bc83047cc
muttalip atasayar <muttalipatasayar@gmail.com>
2021-01-02 22:02:52 +0300

    Add files via upload

---
+++ kok_bulma.py
@@ -0,0 +1,9 @@
+a=int(input("a:"))
+b=int(input("b:"))
+c=int(input("c:"))
+
+delta=b**2-4*a*c
+x1=(-b-delta**0.5)/(2*a)
+x2=(-b+delta**0.5)/(2*a)
+
+print("birinci kök : {}\n ikinci kök : {}\n".format(x1,x2))
